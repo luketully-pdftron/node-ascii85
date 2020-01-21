40672c766200d99f32e9c6012a3acd3ef504c46e
ltully <ltully@pdftron.com>
2020-01-20 17:35:45 -0800

    Remove ZeroMQ specific code

--- index.js
+++ index.js
@@ -362,18 +362,6 @@ Ascii85.prototype.decode = function(str, table) {
 };
 
 /**
- * Ascii85 for ZeroMQ which uses a different codec table.
- */
-defaultCodec.ZeroMQ = new Ascii85({
-  table: [
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-    '.', '-', ':', '+', '=', '^', '!', '/', '*', '?', '&', '<', '>', '(', ')', '[', ']', '{', '}', '@', '%', '$', '#'
-  ]
-});
-
-/**
  * Ascii85 for PostScript which always uses delimiter for encoding.
  */
 defaultCodec.PostScript = new Ascii85({
